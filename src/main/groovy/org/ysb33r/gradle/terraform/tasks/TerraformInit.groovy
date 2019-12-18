e355fba6826084e51474addd30e44e057d6c35fa
Schalk Cronje <ysb33r@gmail.com>
2019-12-18 22:15:19 +0100

    Support `-backend-config` for TerraformInit

--- src/main/groovy/org/ysb33r/gradle/terraform/tasks/TerraformInit.groovy
+++ src/main/groovy/org/ysb33r/gradle/terraform/tasks/TerraformInit.groovy
@@ -16,10 +16,16 @@
 package org.ysb33r.gradle.terraform.tasks
 
 import groovy.transform.CompileStatic
+import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.options.Option
 import org.ysb33r.gradle.terraform.TerraformExecSpec
 import org.ysb33r.gradle.terraform.config.Lock
+import org.ysb33r.grolifant.api.MapUtils
+
+import java.util.concurrent.Callable
 
 /** Equivalent of {@code terraform init}.
  *
@@ -61,8 +67,82 @@ class TerraformInit extends AbstractTerraformTask {
     // TODO: Other backend settings
     // -force-copy
     // -reconfigure
-    // -backend-config
 
+    /** Configuration for Terraform backend.
+     *
+     * See {@link https://www.terraform.io/docs/backends/config.html#partial-configuration}
+     *
+     * @return Location of configuration file. Can be {@code null} if none is required.
+     *
+     * @since 0.4.0
+     */
+    @InputFile
+    @Optional
+    Provider<File> getBackendConfigFile() {
+        this.backendConfig
+    }
+
+    /** Set location of backend configuration file.
+     *
+     * @param location Anythign that can be converted using {@code project.file}.
+     *
+     * @since 0.4.0
+     */
+    void setBackendConfigFile(Object location) {
+        this.backendConfig = location ? project.provider({ ->
+            project.file(location)
+        } as Callable<File>) : null
+    }
+
+    /** Backend configuration files.
+     *
+     * This can be set in addition or as alternative to using a configuration file for the backend.
+     *
+     * @return Map of configuration values. Never {@code null}.
+     *
+     * @since 0.4.0
+     */
+    Map<String, String> getBackendConfigValues() {
+        MapUtils.stringizeValues(this.backendConfigValues)
+    }
+
+    /** Replaces any existing backend configuration values with a new set.
+     *
+     * It does not affect anything specified via a configuration file.
+     *
+     * @param backendValues Map of replacement key-value pairs.
+     *
+     * @since 0.4.0
+     */
+    void setBackendConfigValues(Map<String, Object> backendValues) {
+        this.backendConfigValues.clear()
+        this.backendConfigValues.putAll(backendValues)
+    }
+
+    /** Adds additional backend configuration values
+     *
+     * @param backendValues Map of key-value pairs.
+     *
+     * @since 0.4.0
+     */
+    void backendConfigValues(Map<String, Object> backendValues) {
+        this.backendConfigValues.putAll(backendValues)
+    }
+
+    /** Adds a single backend value.
+     *
+     * @param key Name of backend configuration
+     * @param value Value of backend configuration.
+     *
+     * @since 0.4.0
+     */
+    void backendConfigValue(String key, Object value) {
+        this.backendConfigValues.put(key, value)
+    }
+
+    /** Whether plugins should be verified.
+     *
+     */
     @Internal
     boolean verifyPlugins = true
 
@@ -76,6 +156,7 @@ class TerraformInit extends AbstractTerraformTask {
     @Override
     protected TerraformExecSpec addCommandSpecificsToExecSpec(TerraformExecSpec execSpec) {
         super.addCommandSpecificsToExecSpec(execSpec)
+
         if (project.gradle.startParameter.offline) {
             logger.warn(
                 'Gradle is running in offline mode. ' +
@@ -92,8 +173,19 @@ class TerraformInit extends AbstractTerraformTask {
 
         execSpec.cmdArgs "-backend=${configureBackend}"
         execSpec.cmdArgs "-verify-plugins=${verifyPlugins}"
+
+        getBackendConfigValues().each { String k, String v ->
+            execSpec.cmdArgs "-backend-config=\"$k=$v\""
+        }
+
+        if (this.backendConfig) {
+            execSpec.cmdArgs("-backend-config=${this.backendConfig.get().absolutePath}")
+        }
+
         execSpec
     }
 
 //    private boolean checkVariables = true
+    private Provider<File> backendConfig
+    private final Map<String, Object> backendConfigValues = [:]
 }
