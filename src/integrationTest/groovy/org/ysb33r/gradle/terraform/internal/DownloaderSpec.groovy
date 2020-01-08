f6222c4c41d680aa88bbe677b0c294c080b9125b
Schalk Cronje <ysb33r@gmail.com>
2020-01-09 00:03:24 +0100

    Clean up codenarc violations

--- src/integrationTest/groovy/org/ysb33r/gradle/terraform/internal/DownloaderSpec.groovy
+++ src/integrationTest/groovy/org/ysb33r/gradle/terraform/internal/DownloaderSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2017-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@
 // distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 //
-
 package org.ysb33r.gradle.terraform.internal
 
 import org.gradle.api.Project
@@ -35,32 +34,30 @@ import spock.util.environment.RestoreSystemProperties
 
 import java.nio.file.Files
 
-
 @RestoreSystemProperties
 class DownloaderSpec extends DownloadTestSpecification {
 
     Project project = ProjectBuilder.builder().build()
 
     @IgnoreIf({ DownloadTestSpecification.SKIP_TESTS })
-    def "Download a Terraform distribution" () {
-        given: "A requirement to download Terraform #TERRAFORM_VERSION"
-        Downloader dwn = new Downloader(DownloadTestSpecification.TERRAFORM_VERSION,project)
-        dwn.downloadRoot = new File(project.buildDir,'download')
-//        dwn.baseURI = new File(DownloadTestSpecification.TERRAFORM_CACHE_DIR,'terraform').toURI()
+    def 'Download a Terraform distribution'() {
+        given: 'A requirement to download Terraform #TERRAFORM_VERSION'
+        Downloader dwn = new Downloader(DownloadTestSpecification.TERRAFORM_VERSION, project)
+        dwn.downloadRoot = new File(project.buildDir, 'download')
 
-        when: "The distribution root is requested"
+        when: 'The distribution root is requested'
         File gotIt = dwn.distributionRoot
 
         String binaryName = DownloadTestSpecification.OS.windows ? 'terraform.exe' : 'terraform'
 
-        then: "The distribution is downloaded and unpacked"
+        then: 'The distribution is downloaded and unpacked'
         gotIt != null
-        new File(gotIt,binaryName).exists()
+        new File(gotIt, binaryName).exists()
 
         and: 'The executable has executable rights'
         Files.isExecutable(gotIt.toPath())
 
-        when: "The executable is run to display the help page"
+        when: 'The executable is run to display the help page'
         OutputStream output = new ByteArrayOutputStream()
         ExecResult result = project.exec {
             executable dwn.terraformExecutablePath
@@ -68,10 +65,10 @@ class DownloaderSpec extends DownloadTestSpecification {
             standardOutput output
         }
 
-        then: "No runtime error is expected"
+        then: 'No runtime error is expected'
         result.assertNormalExitValue()
 
-        and: "The expected help information is displayed"
+        and: 'The expected help information is displayed'
         output.toString().startsWith('''Usage: terraform [-version] [-help] <command> [args]''')
     }
 }
--- src/test/groovy/org/ysb33r/gradle/terraform/TerraformPluginSpec.groovy
+++ src/test/groovy/org/ysb33r/gradle/terraform/TerraformPluginSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2017-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import org.ysb33r.gradle.terraform.tasks.TerraformApply
 import org.ysb33r.gradle.terraform.tasks.TerraformInit
 import org.ysb33r.gradle.terraform.tasks.TerraformPlan
 import spock.lang.Specification
-
 
 class TerraformPluginSpec extends Specification {
 
