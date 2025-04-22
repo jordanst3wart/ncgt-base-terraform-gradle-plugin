d15d792c05eb4d1ef06a49f8ed7f179d5d0b24fd
stewart-newscorp <jstewart@newscorp.com>
2025-04-22 12:09:18 +1000

    clean up

---
+++ example/build.gradle.kts
@@ -0,0 +1,23 @@
+
+
+plugins {
+    //id("foo.bar.terraform") version("2.0.0")
+    // id("com.newscorp.gt.gradle.terraform")
+}
+
+// /Users/stewartj/.m2/repository/com/newscorp/gt/gradle/terraform
+// com.newscorp.gt.gradle.terraform.gradle.plugin
+/*terraform {
+    executable version : '1.11.1'
+}
+
+terraformSourceSets {
+    main {
+        srcDir = file('main/tf')
+        backendText("hi") // backend file...
+        variables {
+            file('vars.tfvars')
+        }
+    }
+}*/
+//
---
+++ plugin/build.gradle.kts
@@ -0,0 +1,126 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+
+plugins {
+    `groovy`
+    `codenarc`
+    `java-gradle-plugin`
+    `maven-publish`
+    alias(libs.plugins.grgit)
+}
+
+apply(from = rootProject.file("gradle/plugin-dev.gradle"))
+apply(from = rootProject.file("gradle/codenarc.gradle"))
+
+
+
+fun Map<String, Any?>.getOrSystemEnvOrDefault(key: String, defaultValue: String): String = this.getOrElse(key) {
+    System.getenv().getOrElse(key) {
+        logger.warn("'$key' property is not defined, defaulting to '$defaultValue'")
+        defaultValue
+    }
+}.toString()
+
+val githubUsername = project.properties.getOrSystemEnvOrDefault("GH_USERNAME", "newscorp-ghfb")
+/*
+ * this is a github classic token that requires publishing permissions to write a new package
+ */
+val githubToken = project.properties.getOrSystemEnvOrDefault("GH_PACKAGES_TOKEN", "dummyToken")
+
+fun String.formatBranchName(): String {
+    // Defines the regex pattern for special characters
+    val specialCharsRegex = Regex("[@!#\$%^&*()\\[\\]{}|\\\\/:;\"'<>, ]")
+    return this.replace(specialCharsRegex, "-").uppercase()
+}
+
+fun createVersion(): String {
+    val gitShortHash = grgit.head().abbreviatedId
+    val dateTag = SimpleDateFormat("yyyyMMdd").format(Date())
+    return if (grgit.branch.current().name == "main") {
+        "$dateTag-$gitShortHash"
+    } else {
+        "${grgit.branch.current().name.formatBranchName()}-SNAPSHOT"
+    }
+}
+
+// create a gradle plugin uploading to gradle plugins website
+gradlePlugin {
+    plugins {
+        create("terraformBasePlugin") {
+            id = "foo.bar.terraform.base" // property("ID").toString()
+            implementationClass = "org.ysb33r.gradle.terraform.plugins.TerraformBasePlugin" // property("IMPLEMENTATION_CLASS").toString()
+            version = createVersion()
+            displayName = "Terraform Base Plugin"
+            description = "Provides Terraform extension and tasks. No need to have terraform installed as plugin will take care of caching and installation in a similar fashion as to have Gradle distributions are cached"
+            tags = listOf("terraform")
+            // website = property("WEBSITE").toString()
+            // vcsUrl = property("VCS_URL").toString()
+        }
+        create("terraformPlugin") {
+            id = "foo.bar.terraform" // property("ID").toString()
+            implementationClass = "org.ysb33r.gradle.terraform.plugins.TerraformPlugin" // property("IMPLEMENTATION_CLASS").toString()
+            version = createVersion()
+            displayName = "Terraform Plugin"
+            description = "Provides Terraform extension and tasks. No need to have terraform installed as plugin will take care of caching and installation in a similar fashion as to have Gradle distributions are cached"
+            tags = listOf("terraform")
+            // website = property("WEBSITE").toString()
+            // vcsUrl = property("VCS_URL").toString()
+        }
+        create("terraformRCPlugin") {
+            id = "foo.bar.terraform.rc" // property("ID").toString()
+            implementationClass = "org.ysb33r.gradle.terraform.plugins.TerraformRCPlugin" // property("IMPLEMENTATION_CLASS").toString()
+            version = createVersion()
+            displayName = "Terraform RC Plugin"
+            description = "Deals specifically with the creation of terraformrc files"
+            tags = listOf("terraform")
+            // website = property("WEBSITE").toString()
+            // vcsUrl = property("VCS_URL").toString()
+        }
+        /*
+                terraformBasePlugin {
+            id = 'foo.bar.terraform.base'
+            displayName = 'Terraform Base plugin'
+            description = 'Provides Terraform extension and tasks. No need to have terraform installed as plugin will take care of caching and installation in a similar fashion as to have Gradle distributions are cached'
+            group = 'foo.bar.gradle.terraform'
+        }
+        terraformPlugin {
+            id = 'foo.bar.terraform'
+            displayName = 'Terraform conventions plugin'
+            description = 'Provides Terraform conventions, source sets and predefined tasks'
+            group = 'foo.bar.gradle.terraform'
+        }
+        terraformRCPlugin {
+            id = 'foo.bar.terraform.rc'
+            displayName = 'Terraform Configuration (terraformrc) plugin'
+            description = 'Deals specifically with the creation of terraformrc files'
+            group = 'foo.bar.gradle.terraform'
+        }
+         */
+    }
+}
+
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            credentials {
+                username = githubUsername
+                password = githubToken
+            }
+            uri("https://maven.pkg.github.com/jordanst3wart/gradle-terraform-plugin")
+        }
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed", "standardOut", "standardError")
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
+    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED", "--add-opens=java.base/java.util=ALL-UNNAMED")
+}
+
