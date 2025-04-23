eb4819f12886eb26c4c00d85e1a20472ce4615ee
stewart-newscorp <jstewart@newscorp.com>
2025-04-23 15:33:53 +1000

    fixes

---
+++ lib/src/main/kotlin/org/ysb33r/gradle/terraform/config/Json.kt
@@ -0,0 +1,22 @@
+package org.ysb33r.gradle.terraform.config
+
+class Json : ConfigExtension {
+    override val name: String = "json"
+
+    var enabled: Boolean = false
+
+    /** Command-line parameter for JSON output.
+     *
+     */
+    companion object {
+        private const val JSON_FORMAT = "-json"
+    }
+
+    override fun getCommandLineArgs(): List<String> {
+        return if (enabled) {
+            listOf(JSON_FORMAT)
+        } else {
+            emptyList()
+        }
+    }
+}
---
+++ lib/src/main/kotlin/org/ysb33r/gradle/terraform/errors/MissingConfiguration.kt
@@ -0,0 +1,8 @@
+package org.ysb33r.gradle.terraform.errors
+
+import org.gradle.api.GradleException
+
+class MissingConfiguration : GradleException {
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+}
