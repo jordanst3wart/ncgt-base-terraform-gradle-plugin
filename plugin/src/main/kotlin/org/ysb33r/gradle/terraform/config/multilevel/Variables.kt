f4d3d57472f41bd646df7109cca6752a4638a214
stewart-newscorp <jstewart@newscorp.com>
2025-05-07 18:11:30 +1000

    remove file

--- plugin/src/main/kotlin/org/ysb33r/gradle/terraform/config/multilevel/Variables.kt
+++ plugin/src/main/kotlin/org/ysb33r/gradle/terraform/config/multilevel/Variables.kt
@@ -4,34 +4,47 @@ import org.gradle.api.provider.Provider
 import org.ysb33r.gradle.terraform.config.VariableSpec
 import org.ysb33r.gradle.terraform.errors.ConfigurationException
 import java.io.File
+import java.nio.file.Path
+import kotlin.streams.toList
 
 class Variables(
     private val rootDirResolver: Provider<File>
 ) : VariableSpec {
-
     val name: String = "variables"
-    private val varsFilesPair: VarsFilesPair = VarsFilesPair()
+    val vars: MutableMap<String, Any> = mutableMapOf()
+    val files: MutableList<String> = mutableListOf()
+
+    fun commandLineArgs(root: Path): List<String> {
+        val varList: MutableList<String> = mutableListOf()
+        varList.addAll(fileNames.stream().map { fileName: String ->
+            "-var-file=${root.resolve(fileName).toFile().absolutePath}"
+        }.toList())
+        return varList
+    }
+
+    val fileNames: Set<String>
+        get() = this.files.toSet()
 
     override fun `var`(name: String, value: Any) {
-        varsFilesPair.vars[name] = value
+        vars[name] = value
     }
 
     override fun map(name: String, map: Map<String, *>) {
-        varsFilesPair.vars[name] = map
+        vars[name] = map
     }
 
     override fun list(name: String, val1: Any, vararg vals: Any) {
         val inputs = mutableListOf<Any>(val1)
         inputs.addAll(vals)
-        varsFilesPair.vars[name] = inputs
+        vars[name] = inputs
     }
 
     override fun list(name: String, vals: Iterable<*>) {
-        varsFilesPair.vars[name] = vals.toList()
+        vars[name] = vals.toList()
     }
 
     override fun file(fileName: String) {
-        varsFilesPair.files.add(fileName)
+        files.add(fileName)
     }
 
     override fun getCommandLineArgs(): List<String> {
@@ -40,13 +53,6 @@ class Variables(
                 "This method can only be called when attached to a task extension or a source set"
             )
 
-        return this.varsFilesPair.commandLineArgs(root)
+        return this.commandLineArgs(root)
     }
-
-    override fun toString(): String {
-        return "Terraform variables: ${this.varsFilesPair}"
-    }
-
-    val allVars: VarsFilesPair
-        get() = this.varsFilesPair
 }
